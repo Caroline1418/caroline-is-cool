c0f364800f55b15a53321a69f5a4551a41170f83
Caroline Phillips <Caroline@Carolines-MacBook-Air.local>
2021-06-14 18:45:24 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//adding my imports
+import Navbar from "./component/navbar.jsx";
+import Footer from "./component/footer.jsx";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./styles/index.scss";
 
-export default App;
+ReactDOM.render(
+	<React.Fragment>
+		<Navbar />
+		<Footer />
+	</React.Fragment>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
